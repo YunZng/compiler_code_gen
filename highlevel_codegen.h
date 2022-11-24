08ac33ff08b6faab4806fcc102145927611424ba
Yulun <yulunliss001@gmail.com>
2022-11-24 15:41:07 -0500

    started

--- highlevel_codegen.h
+++ highlevel_codegen.h
@@ -7,7 +7,7 @@
 // A HighLevelCodegen visitor generates high-level IR code for
 // a single function. Code generation is initiated by visiting
 // a function definition AST node.
-class HighLevelCodegen : public ASTVisitor {
+class HighLevelCodegen : public ASTVisitor{
 private:
   int m_next_label_num;
   std::string m_return_label_name; // name of the label that return instructions should target
@@ -15,36 +15,37 @@ private:
   unsigned curVreg;
   unsigned imVreg;
   unsigned argVreg;
+  unsigned highestVreg;
 
 public:
   // the next_label_num controls where the next_label() member function
   HighLevelCodegen(int next_label_num);
   virtual ~HighLevelCodegen();
 
-  std::shared_ptr<InstructionSequence> get_hl_iseq() { return m_hl_iseq; }
+  std::shared_ptr<InstructionSequence> get_hl_iseq(){ return m_hl_iseq; }
 
   int get_next_label_num(){ return m_next_label_num++; }
 
-  virtual void visit_array_element_ref_expression(Node *n);
-  virtual void visit_binary_expression(Node *n);
-  virtual void visit_do_while_statement(Node *n);
+  virtual void visit_array_element_ref_expression(Node* n);
+  virtual void visit_binary_expression(Node* n);
+  virtual void visit_do_while_statement(Node* n);
   // virtual void visit_expression_statement(Node *n);
-  virtual void visit_field_ref_expression(Node *n);
-  virtual void visit_for_statement(Node *n);
-  virtual void visit_function_definition(Node *n);
-  virtual void visit_function_call_expression(Node *n);
-  virtual void visit_if_statement(Node *n);
-  virtual void visit_if_else_statement(Node *n);
-  virtual void visit_indirect_field_ref_expression(Node *n);
-  virtual void visit_literal_value(Node *n);
-  virtual void visit_return_statement(Node *n);
-  virtual void visit_return_expression_statement(Node *n);
-  virtual void visit_unary_expression(Node *n);
-  virtual void visit_variable_ref(Node *n);
-  virtual void visit_while_statement(Node *n);
+  virtual void visit_field_ref_expression(Node* n);
+  virtual void visit_for_statement(Node* n);
+  virtual void visit_function_definition(Node* n);
+  virtual void visit_function_call_expression(Node* n);
+  virtual void visit_if_statement(Node* n);
+  virtual void visit_if_else_statement(Node* n);
+  virtual void visit_indirect_field_ref_expression(Node* n);
+  virtual void visit_literal_value(Node* n);
+  virtual void visit_return_statement(Node* n);
+  virtual void visit_return_expression_statement(Node* n);
+  virtual void visit_unary_expression(Node* n);
+  virtual void visit_variable_ref(Node* n);
+  virtual void visit_while_statement(Node* n);
 
 private:
   std::string next_label();
   Operand next_vr();
-  int get_offset(std::shared_ptr<Type>, std::string);
+  int get_offset(std::shared_ptr<Type> , std::string);
 };
--- sourcefile.c
+++ sourcefile.c
@@ -1,14 +1,5 @@
-
-
-struct Point {
-  int x, y;
-};
-
-int main(void) {
-  struct Point p;
-
-  p.x = 2;
-  p.y = 3;
-
-  return p.x + p.y;
+int main(void){
+  int x;
+  x = 1 + 2;
+  return x;
 }
